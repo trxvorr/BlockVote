630e6b2167efdd81b79f12b404c4dac32fbab1b4
trxvorr <148091717+trxvorr@users.noreply.github.com>
2025-12-15 08:48:11 +0530

    Initialize Blockchain class with basic structure

---
+++ blockchain.py
@@ -0,0 +1,13 @@
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        print("Blockchain initialized.")
+
+    def new_block(self):
+        # This will create a new block
+        pass
+
+    def new_transaction(self):
+        # This will add a new vote
+        pass
